b28d1d2a4f0736fd0db8b9877519200fd35e23b3
Sam Goddard <hello@samgoddard.co.uk>
2022-03-17 16:19:44 +0000

    FPS meter added

--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,7 @@ import { AnimatePresence } from 'framer-motion'
 import { useRouter } from 'next/router'
 import { DefaultSeo } from 'next-seo'
 import SEO from '@/helpers/seo.config'
+import FPSStats from 'react-fps-stats'
 
 export default function App({ Component, pageProps }) {
   const router = useRouter()
@@ -12,6 +13,8 @@ export default function App({ Component, pageProps }) {
       <DefaultSeo {...SEO} />
 
       { pageProps.preview && <><div className={'fixed bottom-0 w-full p-2 bg-red-500 opacity-75 text-white justify-center flex z-50 uppercase font-medium'}>! Preview Mode Enabled - <a className={'px-1 underline'} href={`/api/exit-preview?currentRoute=${router.route}`}>Click Here To Exit</a> !</div></> }
+
+      <FPSStats top="auto" bottom="0" right="0" left="auto" />
 
       <AnimatePresence exitBeforeEnter>
         <Component {...pageProps} key={router.asPath} />
