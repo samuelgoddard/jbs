b9c7ccafffd160c9508abbcc8c73c0d5f92553bf
Sam Goddard <hello@samgoddard.co.uk>
2022-04-12 17:27:20 +0100

    fps monitor

--- pages/_app.js
+++ pages/_app.js
@@ -3,7 +3,7 @@ import { AnimatePresence } from 'framer-motion'
 import { useRouter } from 'next/router'
 import { DefaultSeo } from 'next-seo'
 import SEO from '@/helpers/seo.config'
-// import FPSStats from 'react-fps-stats'
+import FPSStats from 'react-fps-stats'
 import Link from 'next/link'
 
 export default function App({ Component, pageProps }) {
@@ -15,7 +15,7 @@ export default function App({ Component, pageProps }) {
 
       { pageProps.preview && <><div className={'fixed bottom-0 w-full p-2 bg-red-500 opacity-75 text-white justify-center flex z-50 uppercase font-medium'}>! Preview Mode Enabled - <a className={'px-1 underline'} href={`/api/exit-preview?currentRoute=${router.route}`}>Click Here To Exit</a> !</div></> }
 
-      {/* <FPSStats top="auto" bottom="0" right="0" left="auto" /> */}
+      <FPSStats top="auto" bottom="0" left="0" right="auto" />
 
       { router.pathname !== '/' && (
         <div className="ml-auto flex space-x-3 text-sm md:text-base w-auto fixed top-0 right-0 z-50">
