14c16b77734f1ffea764416ea1c3ca890dff7dd2
Sam Goddard <hello@samgoddard.co.uk>
2022-05-17 11:57:10 +0100

    transition timings

--- helpers/transitions.js
+++ helpers/transitions.js
@@ -2,11 +2,11 @@ export const fade = {
 	initial: { opacity: 0 },
   enter: { 
     opacity: 1,
-    transition: { duration: 0.5, ease: [0.76, 0, 0.24, 1] }
+    transition: { duration: 0.65, ease: [0.76, 0, 0.24, 1] }
   },
 	exit: {
     opacity: 0,
-		transition: { duration: 0.4, ease: [0.76, 0, 0.24, 1] }
+		transition: { duration: 0.45, ease: [0.76, 0, 0.24, 1] }
 	}
 }
 
@@ -14,11 +14,11 @@ export const fadeDelay = {
 	initial: { opacity: 0 },
   enter: { 
     opacity: 1,
-    transition: { duration: 0.5, ease: [0.76, 0, 0.24, 1] }
+    transition: { duration: 0.65, ease: [0.76, 0, 0.24, 1] }
   },
 	exit: {
     opacity: 0,
-		transition: { duration: 0.4, ease: [0.76, 0, 0.24, 1] }
+		transition: { duration: 0.45, ease: [0.76, 0, 0.24, 1] }
 	}
 }
 
@@ -26,11 +26,11 @@ export const reveal = {
 	initial: { y: '-100%' },
   enter: { 
     y: 0,
-    transition: { duration: 0.8, ease: [0.76, 0, 0.24, 1] }
+    transition: { duration: 0.65, ease: [0.76, 0, 0.24, 1] }
   },
 	exit: {
     y: '-100%',
-		transition: { duration: 0.5, ease: [0.76, 0, 0.24, 1] }
+		transition: { duration: 0.45, ease: [0.76, 0, 0.24, 1] }
 	}
 }
 
@@ -38,11 +38,11 @@ export const revealDelay = {
 	initial: { y: '-100%' },
   enter: { 
     y: 0,
-    transition: { duration: 0.8, delay: 0, ease: [0.76, 0, 0.24, 1] }
+    transition: { duration: 0.65, ease: [0.76, 0, 0.24, 1] }
   },
 	exit: {
     y: '-100%',
-		transition: { duration: 0.5, ease: [0.76, 0, 0.24, 1] }
+		transition: { duration: 0.45, ease: [0.76, 0, 0.24, 1] }
 	}
 }
 
@@ -50,11 +50,11 @@ export const revealDelayBottom = {
 	initial: { y: '-100%' },
   enter: { 
     y: 0,
-    transition: { duration: 0.8, delay: 0, ease: [0.76, 0, 0.24, 1] }
+    transition: { duration: 0.65, ease: [0.76, 0, 0.24, 1] }
   },
 	exit: {
     y: '-100%',
-		transition: { duration: 0.5, ease: [0.76, 0, 0.24, 1] }
+		transition: { duration: 0.45, ease: [0.76, 0, 0.24, 1] }
 	}
 }
 
@@ -62,22 +62,22 @@ export const revealDelayTop = {
 	initial: { y: '-100%' },
   enter: { 
     y: 0,
-    transition: { duration: 0.8, delay: 0, ease: [0.76, 0, 0.24, 1] }
+    transition: { duration: 0.65, ease: [0.76, 0, 0.24, 1] }
   },
 	exit: {
     y: '-100%',
-		transition: { duration: 0.5, delay: 0, ease: [0.76, 0, 0.24, 1] }
+		transition: { duration: 0.45, ease: [0.76, 0, 0.24, 1] }
 	}
 }
 
 export const scaleDelay = {
-	initial: { scale: 1.125 },
+	initial: { scale: 1.05 },
   enter: { 
     scale: 1,
-    transition: { duration: 0.8, ease: [0.76, 0, 0.24, 1] }
+    transition: { duration: 0.65, ease: [0.76, 0, 0.24, 1] }
   },
 	exit: {
-    scale: 1.125,
-		transition: { duration: 0.5, ease: [0.76, 0, 0.24, 1] }
+    scale: 1.05,
+		transition: { duration: 0.45, ease: [0.76, 0, 0.24, 1] }
 	}
 }
