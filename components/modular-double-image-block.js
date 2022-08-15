b436ea02f0871e49a9117593b1bf0d3d285794b4
Sam Goddard <hello@samgoddard.co.uk>
2022-08-15 12:18:45 +0100

    module udpates

--- components/modular-double-image-block.js
+++ components/modular-double-image-block.js
@@ -1,26 +1,26 @@
 import Image from "@/components/image";
 
 export default function ModularDoubleImageBlock({ image1, image2, image1AspectRatio, image2AspectRatio, reverseOrder }) {
-  let image1Width = 'w-[60%]';
-  let image1Height = 'h-[45vw]';
+  let image1Width = 'w-[90%] md:w-[87%]';
+  let image1Height = 'h-[60vw] md:h-[57vw]';
 
-  let image2Width = 'w-[80%]';
-  let image2Height = 'h-[60vw]';
+  let image2Width = 'w-[90%] md:w-[87%]';
+  let image2Height = 'h-[60vw] md:h-[57vw]';
 
   if (image1AspectRatio == 'square') {
-    image1Width = 'w-[65%]'
-    image1Height = 'h-[28vw]'
+    image1Width = 'w-[85%] md:w-[75%]'
+    image1Height = 'h-[38vw] md:h-[35vw]'
   }
 
   if (image2AspectRatio == 'square') {
-    image2Width = 'w-[65%]'
-    image2Height = 'h-[28vw]'
+    image2Width = 'w-[85%] md:w-[75%]'
+    image2Height = 'h-[38vw] md:h-[35vw]'
   }
 
   return (
-    <div className={`flex flex-wrap items-end p-3 ${reverseOrder ? 'flex-row-reverse' : ' '}`}>
+    <div className={`flex flex-wrap items-end p-3`}>
       <div className={`w-1/2 relative overflow-hidden ${image1AspectRatio == 'square' ? '' : '' }`}>
-        <div className={`${image1Width} relative overflow-hidden ${ reverseOrder ? 'ml-auto' : '' }`}>
+        <div className={`${image1Width} relative overflow-hidden`}>
           <Image
             image={image1}
             focalPoint={image1.hotspot}
@@ -31,7 +31,7 @@ export default function ModularDoubleImageBlock({ image1, image2, image1AspectRa
         </div>
       </div>
       <div className="w-1/2 relative overflow-hidden flex">
-        <div className={`${image2Width} relative overflow-hidden ${ reverseOrder ? 'mr-auto' : 'ml-auto' }`}>
+        <div className={`${image2Width} relative overflow-hidden ml-auto`}>
           <Image
             image={image2}
             focalPoint={image2.hotspot}
--- components/modular-text-block.js
+++ components/modular-text-block.js
@@ -1,9 +1,22 @@
 import BlockContent from '@sanity/block-content-to-react'
 
-export default function ModularTextBlock({ text }) {
+export default function ModularTextBlock({ text, position }) {
+  let cols = 'col-span-8 md:col-span-3';
+  let start = 'md:col-start-0'
+
+  if (position == 'center') {
+    cols = 'col-span-8 md:col-span-3'
+    start = 'md:col-start-4'
+  }
+
+  if (position == 'right') {
+    cols = 'col-span-8 md:col-span-3'
+    start = 'md:col-start-6'
+  }
+
   return (
     <div className="grid grid-cols-9 p-3">
-      <div className="md:col-start-6 col-span-8 md:col-span-3 indent-12">
+      <div className={`${cols} ${start} indent-12`}>
         <BlockContent serializers={{ container: ({ children }) => children }} blocks={text} />
       </div>
     </div>
