5bb7f6455b6b55280f71edd38d980a50f3533e73
Sam Goddard <hello@samgoddard.co.uk>
2022-09-19 15:50:01 +0100

    trello mobile pass

--- components/modular-video-block.js
+++ components/modular-video-block.js
@@ -3,10 +3,11 @@ export default function ModularVideoBlock({ video }) {
     <div className="grid grid-cols-9 md:p-3 border-b border-white md:border-0">
       <div className="col-span-9 md:col-span-7 md:col-start-2">
         <video 
-          controls={true}
+          controls
+          preload="metadata"
           className="w-full h-[66vw] md:h-[44vw] relative z-10 block object-cover object-center"
         >
-          <source src={video} type="video/mp4"/>
+          <source src={`${video}#t=0.5`} type="video/mp4"/>
           Sorry. Your browser does not support the video tag.
         </video>
       </div>
